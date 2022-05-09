d3f93f99844b95278c56be1c00e5b94085e988d9
Vitor <assuncao26vitor@gmail.com>
2022-05-09 20:23:43 -0300

    Instalados Modulos MUI e Styled Components

---
+++ frontend/src/components/Login/styles.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    height: 99.5vh;
+    width: 99.5vw;
+`;
+
+export const LeftContainer = styled.div`
+    width: 50%;
+`;
+
+export const RightContainer = styled.div`
+    width: 50%;
+`;
+
+export const StyledTitle = styled.div`
+    margin-inline: 30%;
+`;
+
+export const StyledWelcomeMessage = styled.div`
+    margin-inline: 30%;
+`;
+
+export const StyledEmailInput = styled.div`
+    margin-inline: 30%;
+`;
+
+export const StyledPasswordInput = styled.div`
+    margin-inline: 30%;
+`;
+
+export const StyledLoginButton = styled.div`
+    margin-inline: 30%;
+`;
