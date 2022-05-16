8d5f731d1dc01149269ad3e8cc3c19cb1ebe7182
Vitor <assuncao26vitor@gmail.com>
2022-05-16 18:33:12 -0300

    Tela Login ajustada para mais resoluções de tela

--- frontend/src/components/Register/Register.tsx
+++ frontend/src/components/Register/Register.tsx
@@ -56,103 +56,98 @@ export function Register () {
     };
 
     return (
-        <div>
+        <Container>
+            <StyledTitle>
+                Cadastro
+            </StyledTitle>
 
-            <Container>
-                    <StyledTitle>
-                        Cadastro
-                    </StyledTitle>
-
-                    <StyledRegisterInfoContainer>
-                        <RowContainer>
-                            <InputContainer> <StyledInput variant="outlined" label="E-mail" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
-                            
-                            <InputContainer> <StyledInput variant="outlined" label="Senha" type="password" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
-                        </RowContainer>
-
-                        <RowContainer>
-                            <InputContainer> <StyledInput variant="outlined" label="Nome Completo" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
-
-                            <InputContainer> <StyledInput variant="outlined" label="Telefone" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
-                        </RowContainer>
-
-                        <div style={{height: "8%"}}>
-                            <StyledFormControlLabel control={
-                                <StyledCheckbox
-                                    checked={checked}
-                                    onChange={handleCheck}
-                                    inputProps={{ 'aria-label': 'controlled' }}
-                                    color="primary"
-                                />
-                            } label="Sou um freelancer"/>
-                        </div>
-
-                        <FreelancerContainer>
-                            {checked? 
-                                <div>
-                                    <p style={{ fontFamily: "'Roboto Condensed', sans-serif",
-                                                fontStyle: "normal",
-                                                fontWeight: "600",
-                                                fontSize: "16px",
-                                                lineHeight: "16px",
-                                                marginBlock: "0",
-                                                color: "#4A5568"}}>
-                                        Habilidades 
-                                    </p>
-
-                                    <List>
-                                        {abilities.map(ability => {
-                                            return(
-                                                <ListItem style={{justifyContent: "space-between", display: "flex", width: "60%"}} key={ability}>
-                                                    <div style={{width: "40%", marginRight: "8px", overflow: "hidden", textOverflow: "ellipsis"}}>
-                                                        <StyledListItemText primary={ability} style={{color: "#2D3748"}}/>
-                                                    </div>
-                                                    <div style={{width: "60%", marginLeft: "4px"}}>
-                                                        <StyledSlider
-                                                            aria-label="Temperature"
-                                                            defaultValue={0}
-                                                            valueLabelDisplay="auto"
-                                                            step={10}
-                                                            marks
-                                                            min={0}
-                                                            max={100}
-                                                            onChange={() => handleSliderChange}
-                                                        />
-                                                    </div>
-                                                </ListItem>
-                                            ) 
-                                        })}
-                                    </List>
-
-                                    <div style={{display: "flex", alignItems: "center"}}>
-                                        {newAbilityInput && 
-                                            <div style={{padding: "0 16px"}}>
-                                                <StyledInput 
-                                                    variant="standard"
-                                                    label="Nova habilidade"
-                                                    value={name}
-                                                    onChange={handleChange}
-                                                    onKeyPress={(ev) => {
-                                                        if (ev.key === 'Enter') {
-                                                            handleAddNewAbility(name)
-                                                        }
-                                                    }}/>
-                                            </div>}
-
-                                        {!newAbilityInput && <StyledAddAbilityButton onClick={handleAddNewAbilityInput}>+</StyledAddAbilityButton>}
-                                    </div>
-                                </div>
-                            : null}
-                        </FreelancerContainer>
-                    </StyledRegisterInfoContainer>
-
-                    <StyledButtonContainer>
-                        <StyledRegisterButton> Cadastrar </StyledRegisterButton>
-                    </StyledButtonContainer>
+            <StyledRegisterInfoContainer>
+                <RowContainer>
+                    <InputContainer> <StyledInput variant="outlined" label="E-mail" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
                     
-            </Container>
+                    <InputContainer> <StyledInput variant="outlined" label="Senha" type="password" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
+                </RowContainer>
 
+                <RowContainer>
+                    <InputContainer> <StyledInput variant="outlined" label="Nome Completo" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
 
-        </div>
+                    <InputContainer> <StyledInput variant="outlined" label="Telefone" InputLabelProps={{ shrink: true, required: true }}/> </InputContainer>
+                </RowContainer>
+
+                <div style={{height: "8%", marginInline: "19%"}}>
+                    <StyledFormControlLabel control={
+                        <StyledCheckbox
+                            checked={checked}
+                            onChange={handleCheck}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                            color="primary"
+                        />
+                    } label="Sou um freelancer"/>
+                </div>
+
+                <FreelancerContainer>
+                    {checked? 
+                        <div>
+                            <p style={{ fontFamily: "'Roboto Condensed', sans-serif",
+                                        fontStyle: "normal",
+                                        fontWeight: "600",
+                                        fontSize: "16px",
+                                        lineHeight: "16px",
+                                        marginBlock: "0",
+                                        marginInline: "19%",
+                                        color: "#4A5568"}}>
+                                Habilidades 
+                            </p>
+
+                            <List style={{marginInline: "19%", marginTop: "12px !important"}}>
+                                {abilities.map(ability => {
+                                    return(
+                                        <ListItem style={{justifyContent: "space-between", display: "flex"}} key={ability}>
+                                            <div style={{width: "40%", marginRight: "8px", overflow: "hidden", textOverflow: "ellipsis"}}>
+                                                <StyledListItemText primary={ability} style={{color: "#2D3748"}}/>
+                                            </div>
+                                            <div style={{width: "60%", marginLeft: "4px"}}>
+                                                <StyledSlider
+                                                    aria-label="Skill Level"
+                                                    defaultValue={0}
+                                                    valueLabelDisplay="auto"
+                                                    step={10}
+                                                    marks
+                                                    min={0}
+                                                    max={100}
+                                                    onChange={() => handleSliderChange}
+                                                />
+                                            </div>
+                                        </ListItem>
+                                    ) 
+                                })}
+                            </List>
+
+                            <div style={{display: "flex", alignItems: "center", marginInline: "19%"}}>
+                                {newAbilityInput && 
+                                    <div style={{padding: "0 16px"}}>
+                                        <StyledInput 
+                                            variant="standard"
+                                            label="Nova habilidade"
+                                            value={name}
+                                            onChange={handleChange}
+                                            onKeyPress={(ev) => {
+                                                if (ev.key === 'Enter') {
+                                                    handleAddNewAbility(name)
+                                                }
+                                            }}/>
+                                    </div>}
+
+                                {!newAbilityInput && <StyledAddAbilityButton onClick={handleAddNewAbilityInput}>+</StyledAddAbilityButton>}
+                            </div>
+                        </div>
+                    : null}
+                </FreelancerContainer>
+            </StyledRegisterInfoContainer>
+
+            <StyledButtonContainer>
+                <StyledRegisterButton> Cadastrar </StyledRegisterButton>
+            </StyledButtonContainer>   
+        </Container>
     );
 }
